b056920fe1160a2268afb946fd2d909772e3e85f
useredsa <emilio.dominguezs@um.es>
2020-03-16 17:37:22 +0100

    added explanation to last problem

--- oj.uz/joi17-abduction2.cpp
+++ oj.uz/joi17-abduction2.cpp
@@ -1,3 +1,31 @@
+/**
+ * O(QN+NlogN) (assuming N>=M)
+ * 
+ * We want to calculate the maximum ammount of movements to get out of grid if we are at
+ * position (s, t) and came from a given direction. If we can do that, we can solve the
+ * problem by choosing the best option out of the 4 possible movements (you can reduce it
+ * to 3 if you see that you can say you started in the cell and then you could go in two
+ * directions).
+ * 
+ * Ok, so now, we will solve: maximum ammount of movements to get out from (s, t) when coming
+ * from direction dir (left, right, down or up).
+ * 
+ * Start with the whole square.
+ * Take the avenue with the greatest value in the current square.
+ * If (s, t) lies in that avenue, then you can find the answer easily.
+ * If (s, t) is not in the avenue, you could find the answer for all of the values on this
+ * avenue and reduce the problem to a smaller square.
+ * 
+ * However, that'd take too long. Fortunately, it's not necessary. Notice that the values
+ * along the edge of your square are something like this:  (9 8 7 7 8) or (5 4 3 4 5), you
+ * just need to calculate the middle point and the value at that point to know the values of
+ * the whole side. And that's basically what I do. To do it:
+ * If for example the highest valued is a vertical street to the left of our point, take the
+ * values of the two intersection points with the upper part and lower part of your square
+ * (the corners if you substitute the left side by this side). With this values you can figure
+ * out the values of this side (in particular, the middle value and where it is).
+ * For more info: look at the code.
+ */
 #include<bits/stdc++.h>
 using namespace std;
 using ll = long long;
@@ -13,41 +41,30 @@ const int MAXN = 50004;
 enum direction{right = 0, up = 1, left = 2, down = 3};
 int n, m, a[MAXN], b[MAXN];
 
-int sparseA[MAXN][20], sparseB[MAXN][20];
-void sparseTable() {
-    for (int i = 0; i < n; i++) {
-        sparseA[i][0] = i;
-    }
-    for (int j = 1; j < 20; j++) {
-        for (int i = 0; i+(1<<j) <= n; i++) {
-            int &l = sparseA[i][j-1], &r = sparseA[i+(1<<(j-1))][j-1];
-            sparseA[i][j] = a[l] > a[r]? l : r;
+struct SparseTable {
+    int *arr;
+    int sp[MAXN][20];
+
+    void build(int *arr, int n) {
+        this->arr = arr;
+        for (int i = 0; i < n; i++) {
+            sp[i][0] = i;
+        }
+        for (int j = 1; j < 20; j++) {
+            for (int i = 0; i+(1<<j) <= n; i++) {
+                int &l = sp[i][j-1], &r = sp[i+(1<<(j-1))][j-1];
+                sp[i][j] = arr[l] > arr[r]? l : r;
+            }
         }
     }
-    for (int i = 0; i < m; i++) {
-        sparseB[i][0] = i;
-    }
-    for (int j = 1; j < 20; j++) {
-        for (int i = 0; i+(1<<j) <= m; i++) {
-            int &l = sparseB[i][j-1], &r = sparseB[i+(1<<(j-1))][j-1];
-            sparseB[i][j] = b[l] > b[r]? l : r;
-        }
-    }
-}
 
-inline int maxa(int l, int r) {
-    int logLen = 0;
-    for (int i = r-l; i > 1; i>>=1) logLen++;
-    int &L = sparseA[l][logLen], &R = sparseA[r-(1<<logLen)][logLen];
-    return a[L] > a[R]? L : R;
-}
-
-inline int maxb(int l, int r) {
-    int logLen = 0;
-    for (int i = r-l; i > 1; i>>=1) logLen++;
-    int &L = sparseB[l][logLen], &R = sparseB[r-(1<<logLen)][logLen];
-    return b[L] > b[R]? L : R;
-}
+    int max(int l, int r) {
+        int logLen = 0;
+        for (int i = r-l; i > 1; i>>=1) logLen++;
+        int &L = sp[l][logLen], &R = sp[r-(1<<logLen)][logLen];
+        return arr[L] > arr[R]? L : R;
+    }
+} sparseA, sparseB;
 
 ll mid[4], midVal[4];
 inline ll dist(direction dir, int id) {
@@ -55,67 +72,65 @@ inline ll dist(direction dir, int id) {
 }
 
 ll solve(int s, int t, direction dir) {
+    using direction::left;
+    using direction::right;
     if (s == -1 || s == n || t == -1 || t == m)
         return 0;
-    
-    using direction::right;
-    using direction::left;
+
+    for (int i = 0; i < 4; i++) {
+        mid[i] = 0;
+        midVal[i] = -1;
+    }
     int l = 0, r = m, d = 0, u = n;
-    mid[0] = mid[1] = mid[2] = mid[3] = 0;
-    midVal[0] = midVal[1] = midVal[2] = midVal[3] = -1;
-    while (true) {
+
+    while (true) { // d < u and l < r
         // cerr << "it [" << d << ", " << u << "]x[" << l << ", " << r << "]" << endl;
         // for (int i = 0; i < 4; i++) {
         //    cerr << mid[i] << " " << midVal[i] << endl;
         // }
-        int maxh = maxa(d, u);
-        int maxv = maxb(l, r);
+        int maxh = sparseA.max(d, u);
+        int maxv = sparseB.max(l, r);
 
         if (b[maxv] > a[maxh]) {
             ll downVal = dist(down, maxv);
             ll upVal   = dist(up, maxv);
             // cerr << "downVal " << downVal << " upVal " << upVal << endl;
-            if (maxv <= t) {
+            if (maxv < t) {
                 l = maxv+1;
                 mid[left] = d-downVal+u-1+upVal;
                 midVal[left] = (-d+downVal+u+upVal+2)/2;
-            } else {
+            } else if (maxv > t) {
                 r = maxv;
                 mid[right] = d-downVal+u-1+upVal;
                 midVal[right] = (-d+downVal+u+upVal+2)/2;
-            }
-
-            if (maxv == t) {
+            } else {
                 if (dir == down) {
                     return upVal+u-s;
                 } else if (dir == up) {
                     return downVal+s-d+1;
                 } else {
-                    assert(max(upVal+u-s, downVal+s-d+1) == dist(left, s));
-                    return dist(left, s);
+                    return max(upVal+u-s, downVal+s-d+1);
                 }
             }
         } else {
             ll leftVal = dist(left, maxh);
             ll rightVal = dist(right, maxh);
             // cerr << "leftVal " << leftVal << " rightVal " << rightVal << endl;
-            if (maxh <= s) {
+            if (maxh < s) {
                 d = maxh+1;
                 mid[down] = l-leftVal+r-1+rightVal;
                 midVal[down] = (-l+leftVal+r+rightVal+2)/2;
-            } else {
+            } else if (maxh > s) {
                 u = maxh;
                 mid[up] = l-leftVal+r-1+rightVal;
                 midVal[up] = (-l+leftVal+r+rightVal+2)/2;
-            }
-            if (maxh == s) {
+            } else {
                 if (dir == left) {
                     return rightVal+r-t;
                 } else if (dir == right) {
                     return leftVal+t-l+1;
                 } else {
-                    assert(max(rightVal+r-t, leftVal+t-l+1) == dist(down, t));
-                    return dist(down, t);
+                    return max(rightVal+r-t, leftVal+t-l+1);
                 }
             }
         }
@@ -134,7 +149,7 @@ int main () {
     for (int i = 0; i < m; i++) {
         cin >> b[i];
     }
-    sparseTable();
+    sparseA.build(a, n), sparseB.build(b, m);
 
     while (q--) {
         cin >> s >> t;
@@ -149,7 +164,6 @@ int main () {
             ans = max(solve(s, t, up)-1, ans);
         }
         cout << ans << '\n';
-        // break;
     }
 }
 
