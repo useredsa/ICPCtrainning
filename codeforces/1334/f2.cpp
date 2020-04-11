57bb3c79604cb2a758f861970f82ddf67a478f66
useredsa <emilio.dominguezs@um.es>
2020-04-12 01:26:57 +0200

    --allow-empty

--- codeforces/1334/f2.cpp
+++ codeforces/1334/f2.cpp
@@ -13,9 +13,7 @@ const int MAXN = 1000006;
 int n, m, a[MAXN], p[MAXN], b[MAXN];
 
 int ind[MAXN], seen[MAXN];
-vi pos[MAXN];
-vll sum[MAXN];
-ll dp[MAXN], negsum[MAXN];
+ll dp[MAXN], sum[MAXN], negsum[MAXN];
 
 int main () {
     ios_base::sync_with_stdio(false);
@@ -38,32 +36,28 @@ int main () {
         while (j < b[i])
             ind[j++] = 2*i-1;
         ind[j++] = 2*i;
-        sum[2*i] = sum[2*i+1] = vll(1);
     }
     for (int i = 0; i < n; ++i) {
         a[i] = ind[a[i]];
-        pos[a[i]].push_back(i);
-        ll prevsum = sum[a[i]].back();
-        sum[a[i]].push_back(prevsum+max(p[i], 0));
+        sum[i] = sum[seen[a[i]]]+max(p[i], 0);
         negsum[i+1] = negsum[i]+min(p[i], 0);
+        seen[a[i]] = i;
     }
 
+    // Dp
     memset(seen, -1, sizeof seen);
-    memset(ind, 0, sizeof ind);
     seen[0] = 0;
     for (int i = 1; i < n; ++i) {
         dp[i] = INF;
         if ((a[i]&1) == 0) {
-            if (int x = seen[a[i]-2]; x != -1) {
-                dp[i] = dp[x]
-                      + sum[a[i]-1][ind[a[i]-1]] + sum[a[i]][ind[a[i]]]
-                      + negsum[i] - negsum[x+1];
-                if (x = seen[a[i]], x != -1)
-                    dp[i] = min(dp[i], dp[x] + negsum[i+1]-negsum[x+1]);
+            if (seen[a[i]-2] != -1) {
+                dp[i] = dp[seen[a[i]-2]] + sum[seen[a[i]-1]] + sum[seen[a[i]]]
+                      + negsum[i] - negsum[seen[a[i]-2]+1];
+                if (seen[a[i]] != -1)
+                    dp[i] = min(dp[i], dp[seen[a[i]]] + negsum[i+1]-negsum[seen[a[i]]+1]);
             }
         }
         seen[a[i]] = i;
-        ind[a[i]]++;
     }
 
     if (dp[n-1] >= INF/2)
